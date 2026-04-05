da6aaac60d78dc21599028320a1f9b0fa3ba6eb9
Krish Jha <jhakrish425@gmail.com>
2026-04-05 23:29:33 +0530

    Add files via upload

---
+++ Day 3/code1.py
@@ -0,0 +1,10 @@
+#Addtion of two numbers
+def add(x,y):
+    res=x+y
+    print(res)
+if __name__=='__main__':
+    a=int(input("Enter value: "))
+    b=int(input("Enter value: "))
+    print(add(a,b))
+   
+   
